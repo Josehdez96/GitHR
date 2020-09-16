f002ffcf47e2a40f592b7bb437c6219fa81806ce
Jose Daniel Hernandez <josedanielhq@gmail.com>
2020-09-15 23:45:15 -0500

    Deleting CandidateMock

--- src/__mocks__/CandidateMock.js
+++
@@ -1,9 +0,0 @@
-const CandidateMock = {
-  NombresApellidos: 'Jose Daniel Hernandez',
-  Cédula: '1037648159',
-  Nacimiento: '15/03/1996',
-  Email: 'josedanielhq@gmail.com',
-  Github: 'josehdez96',
-};
-
-export default CandidateMock;
